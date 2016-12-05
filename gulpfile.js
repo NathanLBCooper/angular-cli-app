c8e035da02d7fed04baf91e9c08e49edf4bfaa64
NathanLBCooper <nathan.lb.cooper@outlook.com>
2016-12-05 11:17:26 +0100

    Add octopus deploy. Everything works :D

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
 const gulp = require('gulp');
+var octo = require("@octopusdeploy/gulp-octo");
 
 gulp.task("hello", function() {
   console.log('Hello world.');
+});
+
+// !! DO NOT COMMIT WITH ACTUAL VALUES !!
+var octoConfig = {
+    host: "TODO, put your host here",
+    apiKey: "TODO, put your api key here"
+};
+
+// You need to bump version in package.json and ng build before running this
+gulp.task("publish", function() {
+    //make sure package.json is not cached
+    delete require.cache[require.resolve("./package.json")];
+
+    return gulp.src(["dist/**"])
+        .pipe(octo.pack())
+        .pipe(octo.push(octoConfig));
 });
